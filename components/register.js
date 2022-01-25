d99f59b321846440732c309e9c769703b63587da
kellcrivelaro <kell_crivelaro@hotmail.com>
2022-01-25 09:07:58 -0300

    first commit

---
+++ components/register.js
@@ -0,0 +1,5 @@
+function Register() {
+  return <h1 className="text-neu-000">Olá</h1>;
+}
+
+export default Register;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Register from "../components/register.js";
+
+export default function Home() {
+  return (
+    <div className="flex rounded-sm bg-neu-800 ring-1 ring-white/10  shadow-md shadow-neu-500 m-4 max-w-2xl">
+      <Register />
+    </div>
+  );
+}
